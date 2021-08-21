76aee9ed689e3324817bb3729f846aa1d53b7ff6
Zach Gilbert <zsgilber@gmail.com>
2021-08-21 15:01:27 -0700

    Change old wire import to shiny new autowire

--- _tutorial/autowire.go
+++ _tutorial/autowire.go
@@ -18,11 +18,13 @@
 // The build tag makes sure the stub is not built in the final build.
 package main
 
-import "github.com/google/wire"
+import (
+	"github.com/dabbertorres/autowire"
+)
 
 // InitializeEvent creates an Event. It will error if the Event is staffed with
 // a grumpy greeter.
 func InitializeEvent(phrase string) (Event, error) {
-	wire.Build(NewEvent, NewGreeter, NewMessage)
+	autowire.Build(NewEvent, NewGreeter, NewMessage)
 	return Event{}, nil
 }
